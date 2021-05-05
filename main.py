347b2cefb43a9ee82ededf8275ed2fc1941f9c1a
Krzysztof Kubicius <kskubicius@gmail.com>
2021-05-05 12:51:11 +0200

    Replaced flask with FastAPI

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from flask import Flask, request
-import requests
+from fastapi import FastAPI
 import json
 from blockchain import Blockchain
 
-app =  Flask(__name__)
+app = FastAPI()
 blockchain = Blockchain()
 
-@app.route('/chain', methods=['GET'])
+@app.get('/chain')
 def get_chain():
     chain_data = []
     for block in blockchain.chain:
         chain_data.append(block.__dict__)
     return json.dumps({"length": len(chain_data),
                       "chain": chain_data})
-app.run(debug=True, port=5000)
+
+def post_transaction():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({"length": len(chain_data),
+                      "chain": chain_data})
